89286b47b6baadfbdf789defdb1da9d3e24c7fca
carla <2191170@my.ipleiria.pt>
2022-04-22 15:00:33 +0100

    AAL backend begin

---
+++ AALBackend/src/main/java/pt/ipleiria/estg/dei/ei/pi/AALBackend/HelloApplication.java
@@ -0,0 +1,9 @@
+package pt.ipleiria.estg.dei.ei.pi.AALBackend;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
