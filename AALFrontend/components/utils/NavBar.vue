590677a43292943bdfa8f78ba2206f6fccae6e72
Rafael Pereira <rafael_pereira_2000@hotmail.com>
2022-04-25 14:23:55 +0100

    Pages Clients + Frames + Profile + Change Password

--- AALFrontend/components/utils/NavBar.vue
+++ AALFrontend/components/utils/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-  <b-navbar toggleable="lg" type="dark" variant="info">
+  <b-navbar toggleable="lg" type="dark" variant="success">
     <b-navbar-brand to="dashboard">
       <img src="../../static/AAL_logo.jpg" class="img-nav-brand" alt="Kitten">
       AAL-Emotion
@@ -10,19 +10,18 @@
 
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav class="ml-auto text-center">
-        <b-nav-item v-if="$auth.user.scope === 'Client'" :class="this.$route.name == 'Frames' ? 'active' : ''" to="Frames">Frames</b-nav-item>
-        <b-nav-item v-if="$auth.user.scope === 'Administrator'" :class="this.$route.name == 'Clients' ? 'active' : ''" to="Clients">Clients</b-nav-item>
+        <b-nav-item v-if="$auth.user.scope === 'Client'" :class="this.$route.name == 'frames' ? 'active' : ''" to="frames">Frames</b-nav-item>
+        <b-nav-item v-if="$auth.user.scope === 'Administrator'" :class="this.$route.name == 'clients' ? 'active' : ''" to="clients">Clients</b-nav-item>
       </b-navbar-nav>
-
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
-        <b-nav-item-dropdown right>
+        <b-nav-item-dropdown right :class="this.$route.name == 'profile' || this.$route.name == 'changepassword' ? 'active' : ''">
           <!-- Using 'button-content' slot -->
           <template #button-content>
             <em>{{currentUser.name}}</em>
           </template>
-          <b-dropdown-item :class="this.$route.name == 'Profile' ? 'active' : ''" to="Profile">Profile</b-dropdown-item>
-          <b-dropdown-item :class="this.$route.name == 'ChangePassword' ? 'active' : ''" to="ChangePassword">Change Password</b-dropdown-item>
+          <b-dropdown-item :active="this.$route.name == 'profile'" to="profile">Profile</b-dropdown-item>
+          <b-dropdown-item :active="this.$route.name == 'changepassword'" to="changepassword">Change Password</b-dropdown-item>
           <b-dropdown-item href="#" @click="logout">Sign Out</b-dropdown-item>
         </b-nav-item-dropdown>
       </b-navbar-nav>
---
+++ AALFrontend/pages/frames.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <navbar/>
+    <h1>Frames</h1>
+  </div>
+</template>
+
+<script>
+import navbar from '~/components/utils/NavBar.vue'
+export default {
+  components: {
+    navbar,
+  },
+  computed: {
+    currentUser(){
+      return this.$auth.user
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
